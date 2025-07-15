5f2fd2b8b9fd3e974cc75d73e1f9b766b34301fc
Roshini Udaya <133715179+RoshiniUdayaKumar@users.noreply.github.com>
2025-07-15 15:07:52 +0530

    Update random_variables.sv

--- Randomization/random_variables.sv
+++ Randomization/random_variables.sv
@@ -1,1 +1,16 @@
+//random values 
+class random ;
+  rand bit [3:0] A;
+  randc bit [3:0] B;  
+endclass
 
+module tb();
+  initial begin
+  random obj;
+    obj=new();
+    repeat(10) begin
+    obj.randomize();
+      $display("Random values for A:%0d and B:%0d", obj.A, obj.B);
+    end
+  end
+endmodule
