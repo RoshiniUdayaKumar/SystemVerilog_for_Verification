8fab3d417597d4e9806e92360f86c8493ff18eff
Roshini Udaya <133715179+RoshiniUdayaKumar@users.noreply.github.com>
2025-07-20 10:45:24 +0530

    Update constraint_block.sv

--- Constraints/Basics/constraint_block.sv
+++ Constraints/Basics/constraint_block.sv
@@ -1,20 +1,3 @@
-class random_value;
-  rand bit[3:0] a;
-  constraint name{a%11==0;}
-endclass
-
-module tb();
-  initial begin
-  random_value obj;
-  obj=new();
-    repeat(10) begin
-      obj.randomize();
-      $display("Randomized numbers that are divisible by 11 are:%0b", obj.a);
-    end
-  end
-endmodule
-
--------------------------------------------------
 //constraint block 
 class packet;
   rand bit[2:0] addr;
